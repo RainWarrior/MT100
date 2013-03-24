d16a1f2b06f4dd56dcdd33a6e9b82a4da039a72b
RainWarrior <rainwarrior@fry.su>
2013-03-24 08:58:26 +0400

    Some parts can be used under LGPL now; mod works in 1.5.1 without any changes

--- rainwarrior/mt100/client/PstFontRegistry.java
+++ rainwarrior/mt100/client/PstFontRegistry.java
@@ -5,25 +5,17 @@ Copyright © 2012 RainWarrior
 This file is part of MT100.
 
 MT100 is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
+it under the terms of the GNU Lesser General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
 MT100 is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+GNU Lesser General Public License for more details.
 
-You should have received a copy of the GNU General Public License
+You should have received a copy of the GNU Lesser General Public License
 along with MT100. If not, see <http://www.gnu.org/licenses/>.
-
-Additional permission under GNU GPL version 3 section 7
-
-If you modify this Program, or any covered work, by linking or combining it
-with Minecraft and/or MinecraftForge (or a modified version of Minecraft
-and/or Minecraft Forge), containing parts covered by the terms of
-Minecraft Terms of Use and/or Minecraft Forge Public Licence, the licensors
-of this Program grant you additional permission to convey the resulting work.
 
 */
 
